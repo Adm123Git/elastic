7f267834ae9c30d6dd54147d0222657208f22f23
Adm123 <adm123@mail.ru>
2022-01-17 10:09:41 +0300

    fix elastic config

--- src/main/java/ru/adm123/elastic/configuration/ESConfiguration.java
+++ src/main/java/ru/adm123/elastic/configuration/ESConfiguration.java
@@ -1,8 +1,6 @@
 package ru.adm123.elastic.configuration;
 
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -17,13 +15,8 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 public class ESConfiguration extends AbstractElasticsearchConfiguration {
 
     @Override
-    @Bean
     public RestHighLevelClient elasticsearchClient() {
-        final ClientConfiguration clientConfiguration = ClientConfiguration
-                        .builder()
-                        .connectedTo("localhost:9200")
-                        .build();
-        return RestClients.create(clientConfiguration).rest();
+        return RestClients.create(ClientConfiguration.localhost()).rest();
     }
 
 }
